b07cbd324d780c36945095b4b71ece0b03b66375
roskonenov <roskonenov@gmail.com>
2025-10-19 19:45:14 +0300

    add Swagger info

--- src/main/java/com/ecommerce/project/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/project/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +25,17 @@ public class SwaggerConfig {
                 .addList("bearerAuth");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot Ecommerce API")
+                        .description("This is a Spring Boot Ecommerce Project")
+                        .contact(new Contact()
+                                .name("Rosen Nenov")
+                                .email("roskonenov@gmail.com")
+                                .url("https://my-portfolio-2e76d.web.app/")
+                        ))
+                .externalDocs(new ExternalDocumentation()
+                        .description("GitHub Project Repository")
+                        .url("https://github.com/roskonenov/Spring-Boot-ECommerce-Project-Back-End"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", securityScheme))
                 .addSecurityItem(bearerAuth);
