b61e2188aedc935e1f4ec2843de2c72571bd7580
roskonenov <roskonenov@gmail.com>
2025-11-29 08:51:45 +0200

    add Redix settings for user authentication

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,6 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./redusers/productReducer";
 import { errorReducer } from "./redusers/errorReducer";
 import { cartReducer } from "./redusers/cartReducer";
+import { authReducer } from "./redusers/authReducer";
+
+const user = localStorage.getItem('auth')
+    ? JSON.parse(localStorage.getItem('auth'))
+    : [];
 
 const cartItems = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
@@ -11,11 +16,13 @@ export const store = configureStore({
     reducer: {
         products: productReducer,
         errors: errorReducer,
-        carts: cartReducer
+        carts: cartReducer,
+        auth: authReducer,
     },
     preloadedState: {
-        carts: {cart: cartItems}
-    }
+        carts: {cart: cartItems},
+        auth: {user: user},
+    },
 });
 
 export default store;
