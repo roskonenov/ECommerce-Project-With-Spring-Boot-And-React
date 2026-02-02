ab38d0b0a12f913c9d1d02829f0a58622638a7a6
roskonenov <roskonenov@gmail.com>
2026-02-02 21:07:10 +0200

    add Admin Orders Pagination settings and data

---
+++ frontend/src/components/helper/AdminOrdersTableColumns.jsx
@@ -0,0 +1,86 @@
+import { FaEdit } from "react-icons/fa";
+
+export const adminOrdersTableColumns = [
+    {
+        sortable: false,
+        disableColumnMenu: true,
+        field: 'id',
+        headerName: 'OrderId',
+        minWidth: 130,
+        headerAlign: 'center',
+        editable: false,
+        headerClassName: 'text-black font-semibold border',
+        cellClassName: 'text-slate-700 font-normal border',
+        renderHeader: (params) => <span>Order ID</span>
+    },
+    {
+        sortable: false,
+        disableColumnMenu: true,
+        field: 'email',
+        headerName: 'Email',
+        width: 250,
+        headerAlign: 'center',
+        editable: false,
+        headerClassName: 'text-black font-semibold border',
+        cellClassName: 'text-slate-700 font-normal border',
+        renderHeader: (params) => <span>Email</span>
+    },
+    {
+        sortable: true,
+        disableColumnMenu: true,
+        field: 'totalAmount',
+        headerName: 'Total Amount',
+        width: 200,
+        headerAlign: 'center',
+        editable: false,
+        headerClassName: 'text-black font-semibold border',
+        cellClassName: 'text-slate-700 font-normal border',
+        renderHeader: (params) => <span>Total Amount</span>
+    },
+    {
+        sortable: false,
+        disableColumnMenu: true,
+        field: 'status',
+        headerName: 'Status',
+        width: 200,
+        headerAlign: 'center',
+        editable: false,
+        headerClassName: 'text-black font-semibold border',
+        cellClassName: 'text-slate-700 font-normal border',
+        renderHeader: (params) => <span>Status</span>
+    },
+    {
+        sortable: false,
+        disableColumnMenu: true,
+        field: 'date',
+        headerName: 'orderDate',
+        width: 200,
+        headerAlign: 'center',
+        editable: false,
+        headerClassName: 'text-black font-semibold border',
+        cellClassName: 'text-slate-700 font-normal border',
+        renderHeader: (params) => <span>Order Date</span>
+    },
+    {
+        sortable: false,
+        disableColumnMenu: true,
+        field: 'action',
+        headerName: 'action',
+        width: 250,
+        headerAlign: 'center',
+        editable: false,
+        headerClassName: 'text-black font-semibold text-center',
+        cellClassName: 'text-slate-700 font-normal',
+        renderHeader: (params) => <span>Action</span>,
+        renderCell: (params) => {
+            return (
+                <div className='flex justify-center items-center space-x-4 h-full '>
+                    <button className='flex justify-center items-center bg-blue-400 text-white px-4 h-9 rounded-md text-shadow-md cursor-pointer'>
+                        <FaEdit className='mr-2' />
+                        Edit
+                    </button>
+                </div>
+            );
+        }
+    },
+];
